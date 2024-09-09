e569643573280327de55252df337cb216e75397f
Gray Lin <gray.lin@primax.com.tw>
2024-09-09 15:11:51 +0800

    u

--- AICamera/x.sh
+++ AICamera/x.sh
@@ -45,21 +45,21 @@ if [ "$1" == "aic" ] ; then
 	appDir="vision_box_DualCam_gray"
 	cd ~/primax/apps
 
-	if [ "$2" = "jobs" ] ; then
+	if [ "$2" == "jobs" ] ; then
 		pm2 list
 
-	elif [ $2 = "git" ] ; then
+	elif [ $2 == "git" ] ; then
 		echo "update src..."
 		cd $appDir 
 		git reset --hard HEAD
 		git pull
 
-	elif [ $2 = "b" ] ; then
+	elif [ $2 == "b" ] ; then
 		echo "Build..."
 		cd $appDir 
 		make
 
-	elif [ $2 = "r" ] ; then
+	elif [ $2 == "r" ] ; then
 		echo "Run..."
 		cd $appDir 
 		./vision_box_DualCam
