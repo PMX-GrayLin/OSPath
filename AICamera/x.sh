80f095ce6524b600687bcce2028f3676ce9ac116
Gray Lin <gray.lin@primax.com.tw>
2024-10-21 16:00:54 +0800

    z

--- AICamera/x.sh
+++ AICamera/x.sh
@@ -41,9 +41,9 @@ if [ "$1" = "aic" ] ; then
 		pm2 list
 
 	elif [ "$2" = "pid" ] ; then
-		echo "PID : $(ps -C vision_box_DualCam)"
-		$(ps -C vision_box_DualCam)
-		
+		echo "$(ps -C vision_box_DualCam)"
+		ps -C vision_box_DualCam
+
 	# elif [ "$2" = "git" ] ; then
 	# 	echo "update src..."
 	# 	cd $appDir 
