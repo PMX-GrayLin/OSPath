a03531bb1729947c817b341a39b3922b974af79a
Gray Lin <llkkqq@gmail.com>
2024-11-03 17:57:26 +0800

    a

--- Ubuntu_DellServer/x.sh
+++ Ubuntu_DellServer/x.sh
@@ -505,6 +505,7 @@ if [ "$1" = "user" ] ; then
 			# make a yocto build dir & user link
 			sudo mkdir /mnt/disk2/yocto_build_folder/$3
 			sudo chown $3:$mainGroup /mnt/disk2/yocto_build_folder/$3
+			cp ../misc/step* .
 			cd /home/$3
 			sudo ln -s /mnt/disk2/yocto_build_folder/$3 yocto_build_folder
 			sudo chown $3:$mainGroup yocto_build_folder
