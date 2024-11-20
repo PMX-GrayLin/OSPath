787f1c5e5f6afc047e293e90c7c8061e491033a8
Gray Lin <gray.lin@primax.com.tw>
2024-11-20 18:42:25 +0800

    z

--- Ubuntu_DellServer/x.sh
+++ Ubuntu_DellServer/x.sh
@@ -60,8 +60,8 @@ if [ "$1" = "bb" ] ; then
 		echo "build recipe... $3"
 		bitbake $3
 
-	elif [ "$2" = "co" ] ; then
-		echo "compile only recipe... $3, bitbake $3 -c compile -f"
+	elif [ "$2" = "oc" ] ; then
+		echo "only compile recipe... $3, bitbake $3 -c compile -f"
 		bitbake $3 -c compile -f
 
 	elif [ "$2" = "i" ] ; then
