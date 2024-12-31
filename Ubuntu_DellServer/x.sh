d739bc6c761a608c08694e7aebfdf12599b060cc
Gray Lin <gray.lin@primax.com.tw>
2024-12-31 17:33:00 +0800

    z

--- Ubuntu_DellServer/x.sh
+++ Ubuntu_DellServer/x.sh
@@ -144,7 +144,6 @@ if [ "$1" = "aic" ] ; then
 		dir_work="$PROJ_ROOT/build/tmp/work/$targetPlatform/primax/1.0-r0"
 		cp -f $dir_work/temp/log.do_compile $dir_ftp
 		cp -f $dir_work/primax-1.0/src/vision_box_DualCam/vision_box_DualCam $dir_ftp
-
 		cp -f $dir_work/primax-1.0/src/Test_C_yocto/test $dir_ftp
 
 		# dir_image="$PROJ_ROOT/build/tmp//deploy/images/genio-700-evk"
