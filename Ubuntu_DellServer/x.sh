cfe59fd207ea0f623861cc241977cdccf4ac719b
Gray Lin <gray.lin@primax.com.tw>
2025-03-25 18:51:09 +0800

    z

--- Ubuntu_DellServer/x.sh
+++ Ubuntu_DellServer/x.sh
@@ -61,11 +61,12 @@ if [ "$1" = "bb" ] ; then
 		bitbake $3
 
 	elif [ "$2" = "ocv" ] ; then
-		echo "only compile recipe... $3, SKIP_VISION_BOX=0 bitbake $3 -c compile"
+		echo "only compile recipe... $3, bitbake $3 -c compile"
+		export SKIP_VISION_BOX="0"
 		bitbake $3 -c compile
 
 	elif [ "$2" = "oct" ] ; then
-		echo "only compile recipe... $3, SKIP_VISION_BOX=1 bitbake $3 -c compile"
+		echo "only compile recipe... $3, bitbake $3 -c compile"
 		export SKIP_VISION_BOX="1"
 		bitbake $3 -c compile
 
