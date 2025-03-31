0424166de13639676aa80ef9b38d0a1cc28df179
Gray Lin <gray.lin@primax.com.tw>
2025-03-31 14:10:27 +0800

    z

--- Ubuntu_DellServer/x.sh
+++ Ubuntu_DellServer/x.sh
@@ -134,7 +134,7 @@ if [ "$1" = "aic" ] ; then
 		git pull
 
 		# make build tag
-		touch $WORKDIR/temp/tag_Build_test
+		touch $WORKDIR/temp/tag_build_test
 
 	elif [ "$2" = "us" ] ; then
 		echo "========== update yocto primax src =========="
@@ -143,7 +143,7 @@ if [ "$1" = "aic" ] ; then
 		git pull
 
 		# make build tag
-		touch $WORKDIR/temp/tag_Build_visionBox
+		touch $WORKDIR/temp/tag_build_visionBox
 
 	elif [ "$2" = "ftp" ] ; then
 		echo "========== update files to FTP =========="
