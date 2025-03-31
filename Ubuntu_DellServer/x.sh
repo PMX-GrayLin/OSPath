5d72f5464aaba37c4c219fe09760d1bd2ce68d5a
Gray Lin <gray.lin@primax.com.tw>
2025-03-31 14:21:09 +0800

    z

--- Ubuntu_DellServer/x.sh
+++ Ubuntu_DellServer/x.sh
@@ -58,6 +58,14 @@ if [ "$1" = "bb" ] ; then
 		
 	elif [ "$2" = "b" ] ; then
 		echo "build recipe... $3"
+
+		# make build tag
+		if [  "$3" = "primax" ] ; then
+			WORKDIR="/mnt/disk2/yocto_build_folder/gray/C_AI-Camera-G2_FW_Yocto/build/tmp/work/armv8a-poky-linux/primax/1.0-r0"
+			touch $WORKDIR/temp/tag_build_test
+			touch $WORKDIR/temp/tag_build_visionBox
+		fi
+
 		bitbake $3
 
 	elif [ "$2" = "ocv" ] ; then
