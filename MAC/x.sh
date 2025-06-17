35cabd653afbc0b0794050062cf2b62e9f3f8e58
Gray Lin <gray.lin@primax.com.tw>
2025-06-17 19:32:30 +0800

    z

--- MAC/x.sh
+++ MAC/x.sh
@@ -26,13 +26,13 @@ wheeltec_ip="192.168.1.196"
 
 # ai camera
 
-AICamera_ip="192.168.1.13"
+AICamera_ip="192.168.1.17"
 # AICamera_ip="192.168.1.65"
 # AICamera_ip="192.168.1.66"
 # AICamera_ip="192.168.1.67"
-AICamera_ip="192.168.1.70"
+# AICamera_ip="192.168.1.70"
 # AICamera_ip="192.168.1.99"
-AICamera_ip="192.168.1.190"
+# AICamera_ip="192.168.1.190"
 
 DellServer_ip="10.1.13.207"
 
