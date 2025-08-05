d4762c11e3039c435a36af3433caa52b1b801cd6
Gray Lin <gray.lin@primax.com.tw>
2025-08-05 11:28:43 +0800

    z

--- MAC/x.sh
+++ MAC/x.sh
@@ -33,7 +33,7 @@ AICamera_ip="aicamera-d14b"
 # AICamera_ip="aicamera-d14b.local"
 # AICamera_ip="visionhub-d14b"
 # AICamera_ip="visionhub-d14b.local"
-# AICamera_ip="192.168.1.205"
+AICamera_ip="192.168.1.59"
 
 
 DellServer_ip="10.1.13.207"
