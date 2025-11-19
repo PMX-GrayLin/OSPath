02e4cf1fdc1f07337856fdf524fa7bdf6c912b70
Gray Lin <gray.lin@primax.com.tw>
2025-11-19 15:14:32 +0800

    a

--- MAC/x.sh
+++ MAC/x.sh
@@ -26,7 +26,7 @@ AICamera_ip="192.168.1.65"
 AICamera_ip="aicamera-0687.local"
 # AICamera_ip="visionhub-d14b"
 # AICamera_ip="visionhub-d14b.local"
-# AICamera_ip="192.168.1.245"
+# AICamera_ip="192.168.1.151"
 
 # nfs
 if [ "$1" == "nfs" ] ; then
